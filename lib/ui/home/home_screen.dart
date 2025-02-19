6d3eb8b0eaa496c8a97a26719ad668be1f23deb0
LSViana <lv201122@gmail.com>
2025-02-18 22:43:48 -0300

    Add `HomeScreen` and `HomeViewModel` for bill listing

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:collection';
+
+import 'package:bills_reminder/domain/models/bill.dart';
+import 'package:bills_reminder/ui/core/bills/bill_list_view.dart';
+import 'package:bills_reminder/ui/home/home_view_model.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required HomeViewModel viewModel})
+    : _viewModel = viewModel;
+
+  final HomeViewModel _viewModel;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    final homeViewModel = widget._viewModel;
+
+    homeViewModel.getBills();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bills Reminder')),
+      body: StreamBuilder<UnmodifiableListView<Bill>>(
+        stream: widget._viewModel.bills,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final bills = snapshot.data!;
+
+          if (bills.isEmpty) {
+            return const Center(child: Text('No bills found'));
+          }
+
+          return BillListView(bills: bills);
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/home/home_view_model.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:bills_reminder/data/repositories/bills/bills_repository.dart';
+import 'package:bills_reminder/domain/models/bill.dart';
+import 'package:logging/logging.dart';
+
+class HomeViewModel {
+  HomeViewModel({required BillsRepository repository})
+    : _repository = repository,
+      _billsController = StreamController<UnmodifiableListView<Bill>>();
+
+  final BillsRepository _repository;
+  final StreamController<UnmodifiableListView<Bill>> _billsController;
+
+  final _log = Logger('HomeViewModel');
+
+  Stream<UnmodifiableListView<Bill>> get bills => _billsController.stream;
+
+  Future<void> getBills() async {
+    _log.fine('Bills loading');
+
+    Future.delayed(const Duration(seconds: 1));
+
+    final bills = await _repository.getBills();
+    _billsController.add(UnmodifiableListView(bills));
+
+    _log.fine('Bills loaded: ${bills.length}');
+  }
+}
