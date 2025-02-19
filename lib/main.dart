fe07cf4585295b187ac7064008d760450e74726b
LSViana <lv201122@gmail.com>
2025-02-18 23:01:15 -0300

    Configure logging for the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:bills_reminder/dependencies/local_providers.dart';
 import 'package:bills_reminder/routing/router.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    debugPrint(
+      '${rec.loggerName} ${rec.level.name} [${rec.time.toIso8601String()}]: ${rec.message}',
+    );
+  });
+
   runApp(const MainApp());
 }
 
