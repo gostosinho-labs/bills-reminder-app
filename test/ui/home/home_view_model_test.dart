491a02118a6cc90a58cca592c987a3e0e5207ecd
LSViana <lv201122@gmail.com>
2025-02-18 22:50:03 -0300

    Add unit test for `HomeViewModel` to verify bill retrieval

---
+++ test/ui/home/home_view_model_test.dart
@@ -0,0 +1,18 @@
+import 'package:bills_reminder/testing/fakes/repositories/fake_booking_repository.dart';
+import 'package:bills_reminder/ui/home/home_view_model.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('get bills', () async {
+    // Arrange
+    final repository = FakeBookingRepository();
+    final viewModel = HomeViewModel(repository: repository);
+
+    // Act
+    await viewModel.getBills();
+    final bills = await viewModel.bills.first;
+
+    // Assert
+    expect(bills.length, 2);
+  });
+}
