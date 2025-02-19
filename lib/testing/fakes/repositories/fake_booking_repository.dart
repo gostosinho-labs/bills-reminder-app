70b20e1d0eb85cbba4bfd09ea92b93b818436ac0
LSViana <lv201122@gmail.com>
2025-02-18 23:15:41 -0300

    Refactor `FakeBookingRepository` to use a mutable list of `Bill` values

--- lib/testing/fakes/repositories/fake_booking_repository.dart
+++ lib/testing/fakes/repositories/fake_booking_repository.dart
@@ -4,25 +4,32 @@ import 'package:bills_reminder/data/repositories/bills/bills_repository.dart';
 import 'package:bills_reminder/domain/models/bill.dart';
 
 class FakeBookingRepository implements BillsRepository {
+  final List<Bill> _bills = [
+    Bill(
+      name: 'Electricity',
+      date: DateTime.now(),
+      notification: true,
+      recurrence: true,
+      paid: false,
+      value: 100,
+    ),
+    Bill(
+      name: 'Gas',
+      date: DateTime.now().add(const Duration(days: 1)),
+      notification: true,
+      recurrence: true,
+      paid: false,
+      value: 120,
+    ),
+  ];
+
   @override
   Future<UnmodifiableListView<Bill>> getBills() async {
-    return UnmodifiableListView([
-      Bill(
-        name: 'Electricity',
-        date: DateTime.now(),
-        notification: true,
-        recurrence: true,
-        paid: false,
-        value: 100,
-      ),
-      Bill(
-        name: 'Gas',
-        date: DateTime.now()..add(const Duration(days: 1)),
-        notification: true,
-        recurrence: true,
-        paid: false,
-        value: 120,
-      ),
-    ]);
+    return UnmodifiableListView(_bills);
+  }
+
+  @override
+  Future<void> addBill(Bill bill) async {
+    _bills.add(bill);
   }
 }
