8b915bee2006d10d881aac56e6cd28ef141e5328
LSViana <lv201122@gmail.com>
2025-02-23 22:35:02 -0300

    Implement bill retrieval and update methods in BillsService

--- lib/data/services/bills/bills_service_database.dart
+++ lib/data/services/bills/bills_service_database.dart
@@ -5,6 +5,26 @@ import 'package:sqflite/sqflite.dart';
 
 class BillsServiceDatabase implements BillsService {
   @override
+  Future<List<Bill>> getBills() async {
+    final database = await BillsDatabase.instance.database;
+    final List<Map<String, dynamic>> maps = await database.query('bills');
+
+    return List.generate(maps.length, (i) => Bill.fromMap(maps[i]));
+  }
+
+  @override
+  Future<Bill> getBill(String id) async {
+    final database = await BillsDatabase.instance.database;
+    final List<Map<String, dynamic>> maps = await database.query(
+      'bills',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    return Bill.fromMap(maps.first);
+  }
+
+  @override
   Future<void> addBill(Bill bill) async {
     final database = await BillsDatabase.instance.database;
 
@@ -16,11 +36,15 @@ class BillsServiceDatabase implements BillsService {
   }
 
   @override
-  Future<List<Bill>> getBills() async {
+  Future<void> updateBill(Bill bill) async {
     final database = await BillsDatabase.instance.database;
-    final List<Map<String, dynamic>> maps = await database.query('bills');
 
-    return List.generate(maps.length, (i) => Bill.fromMap(maps[i]));
+    await database.update(
+      'bills',
+      bill.toMap(),
+      where: 'id = ?',
+      whereArgs: [bill.id],
+    );
   }
 
   @override
