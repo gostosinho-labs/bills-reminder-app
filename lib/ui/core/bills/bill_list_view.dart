8e96a5f06397a45624cdd0df2257911aefb937b1
LSViana <lv201122@gmail.com>
2025-03-18 22:32:16 -0300

    Add month header to BillListView for improved organization
    - Introduced a month header in BillListView to group bills by their due month, enhancing visual organization and user experience when viewing bills in the list.

--- lib/ui/core/bills/bill_list_view.dart
+++ lib/ui/core/bills/bill_list_view.dart
@@ -3,6 +3,7 @@ import 'dart:collection';
 import 'package:bills_reminder/domain/models/bill.dart';
 import 'package:bills_reminder/ui/core/bills/bill_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BillListView extends StatelessWidget {
   const BillListView({super.key, required this.bills, required this.onEdit});
@@ -12,12 +13,36 @@ class BillListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dateFormat = DateFormat('MMMM');
+
+    String? currentMonth;
+
     return ListView.builder(
       itemCount: bills.length,
       itemBuilder: (context, index) {
         final bill = bills[index];
+        final month = dateFormat.format(bill.date);
 
-        return BillListItem(bill: bill, onTap: () => onEdit(bill));
+        final bool showMonth = currentMonth != month;
+
+        if (showMonth) {
+          currentMonth = month;
+        }
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (showMonth)
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  month,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+            BillListItem(bill: bill, onTap: () => onEdit(bill)),
+          ],
+        );
       },
     );
   }
