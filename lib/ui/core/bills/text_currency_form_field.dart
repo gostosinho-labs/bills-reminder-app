b25b5d2fb5218795e16fdcaf2d62053e1728332c
LSViana <lv201122@gmail.com>
2025-03-30 23:21:17 -0300

    Add TextCurrencyFormField widget for formatted currency input
    - Introduced TextCurrencyFormField to handle currency input with automatic formatting.
    - Ensured initial value is set to '0.00' if the controller is empty.
    - Implemented input sanitization to allow only numeric characters and format the value as currency.
    - Enhanced user experience with a clear label and helper text in the input field.

---
+++ lib/ui/core/bills/text_currency_form_field.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TextCurrencyFormField extends StatefulWidget {
+  const TextCurrencyFormField({super.key, required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  State<TextCurrencyFormField> createState() => _TextCurrencyFormFieldState();
+}
+
+class _TextCurrencyFormFieldState extends State<TextCurrencyFormField> {
+  @override
+  void initState() {
+    super.initState();
+
+    widget.controller.value = TextEditingValue(
+      text: widget.controller.text.isEmpty ? '0.00' : widget.controller.text,
+      selection: TextSelection.collapsed(offset: '0.00'.length),
+    );
+  }
+
+  void _onChanged(String value) {
+    // Remove non-numeric characters
+    String cleanValue = value.replaceAll(RegExp(r'[^0-9]'), '');
+
+    // Parse the cleaned value as an integer
+    int parsedValue = int.tryParse(cleanValue) ?? 0;
+
+    // Format the value as currency (without symbol)
+    final formattedValue = (parsedValue / 100).toStringAsFixed(2);
+
+    // Update the text field with the formatted value
+    widget.controller.value = TextEditingValue(
+      text: formattedValue,
+      selection: TextSelection.collapsed(offset: formattedValue.length),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        labelText: 'Bill Value',
+        border: OutlineInputBorder(),
+        helperText: 'Optional, leave empty for variable value',
+      ),
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      onChanged: _onChanged,
+    );
+  }
+}
