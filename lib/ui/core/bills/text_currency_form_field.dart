f7f3a147a30603125717281edc108a1827568cce
LSViana <lv201122@gmail.com>
2025-03-30 23:25:57 -0300

    Enhance TextCurrencyFormField with customizable label and helper text
    - Added optional labelText and helperText parameters to TextCurrencyFormField for improved user guidance.
    - Updated the build method to utilize these parameters in the TextFormField decoration, enhancing the overall user experience.

--- lib/ui/core/bills/text_currency_form_field.dart
+++ lib/ui/core/bills/text_currency_form_field.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class TextCurrencyFormField extends StatefulWidget {
-  const TextCurrencyFormField({super.key, required this.controller});
+  const TextCurrencyFormField({
+    super.key,
+    required this.controller,
+    this.labelText,
+    this.helperText,
+  });
 
   final TextEditingController controller;
+  final String? labelText;
+  final String? helperText;
 
   @override
   State<TextCurrencyFormField> createState() => _TextCurrencyFormFieldState();
@@ -18,6 +25,21 @@ class _TextCurrencyFormFieldState extends State<TextCurrencyFormField> {
     widget.controller.value = TextEditingValue(
       text: widget.controller.text.isEmpty ? '0.00' : widget.controller.text,
       selection: TextSelection.collapsed(offset: '0.00'.length),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        border: const OutlineInputBorder(),
+        helperText: widget.helperText,
+      ),
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      onChanged: _onChanged,
     );
   }
 
@@ -35,21 +57,6 @@ class _TextCurrencyFormFieldState extends State<TextCurrencyFormField> {
     widget.controller.value = TextEditingValue(
       text: formattedValue,
       selection: TextSelection.collapsed(offset: formattedValue.length),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: widget.controller,
-      keyboardType: TextInputType.number,
-      decoration: const InputDecoration(
-        labelText: 'Bill Value',
-        border: OutlineInputBorder(),
-        helperText: 'Optional, leave empty for variable value',
-      ),
-      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      onChanged: _onChanged,
     );
   }
 }
